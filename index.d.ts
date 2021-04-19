440d6073d851c53ef9c96b311dfc85d206edb56d
Yogendra Singh <yog27ray@gmail.com>
2021-04-19 16:22:26 +0530

    v1.0.7

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
-import { Application, AOPController, Controller, Factory, AOPMiddleware, Middleware, AOPModel, AOPModule, Module, AOPService, Service, API_REQUEST, Base, DELETE, GET, MiddlewareRequest, POST, PUT } from './src';
-export { Service, Factory, Application, Controller, Middleware, Module, GET, PUT, POST, DELETE, API_REQUEST, MiddlewareRequest, AOPService, AOPModule, AOPController, AOPModel, Base, AOPMiddleware, };
+import { AOPApplication, AOPController, AOPMiddleware, AOPModel, AOPModule, AOPService, API_REQUEST, Application, Base, Controller, DELETE, Factory, GET, Middleware, MiddlewareRequest, Module, POST, PUT, Service } from './src';
+export { AOPApplication, AOPController, AOPMiddleware, AOPModel, AOPModule, AOPService, API_REQUEST, Application, Base, Controller, DELETE, Factory, GET, Middleware, MiddlewareRequest, Module, POST, PUT, Service, };
