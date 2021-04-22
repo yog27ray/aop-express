299f4f9d98ccb8a7262f6e5732999153a95431bf
Yogendra Singh <yog27ray@gmail.com>
2021-04-22 16:45:24 +0530

    fix: provide getProvide in provider function

--- index.ts
+++ index.ts
@@ -1,17 +1,19 @@
 import {
   AOPApplication, AOPController, AOPMiddleware,
   AOPModule,
-  AOPRequest,
-  AOPResponse, AOPService, Application, Base,
-  Controller, DELETE,
+  AOPProvider,
+  AOPRequest, AOPResponse, AOPService, Application,
+  Base, Controller,
+  DELETE,
   GET, Middleware, MiddlewareRequest, Module, POST, Provider, PUT, Service,
 } from './src';
 
 export {
   AOPApplication, AOPController, AOPMiddleware,
   AOPModule,
-  AOPRequest,
-  AOPResponse, AOPService, Application, Base,
-  Controller, DELETE,
+  AOPProvider,
+  AOPRequest, AOPResponse, AOPService, Application,
+  Base, Controller,
+  DELETE,
   GET, Middleware, MiddlewareRequest, Module, POST, Provider, PUT, Service,
 };
