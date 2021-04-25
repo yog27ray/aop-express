d03baeb29c1e982de76723f3e04c62b4946d1072
Yogendra Singh <yog27ray@gmail.com>
2021-04-26 01:48:49 +0530

    v1.0.34

--- src/declarations/provider.d.ts
+++ src/declarations/provider.d.ts
@@ -1,4 +1,6 @@
 import { Base } from './base';
+import { Factory } from './factory';
 export declare class Provider extends Base {
+    protected getFactory<T extends Factory>(table: new () => T): T;
     protected getProvider<T>(table: new () => T): T;
 }
--- src/declarations/provider.js
+++ src/declarations/provider.js
@@ -4,6 +4,9 @@ exports.Provider = void 0;
 const base_1 = require("./base");
 const inversify_1 = require("./inversify");
 class Provider extends base_1.Base {
+    getFactory(table) {
+        return inversify_1.factoryContainer.get(table);
+    }
     getProvider(table) {
         return inversify_1.providerContainer.get(table);
     }
